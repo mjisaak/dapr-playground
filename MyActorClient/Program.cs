af27f2cea7615e43203f8c144b32fe778e67964a
Martin Brandl <mjibrandl@gmail.com>
2020-05-25 08:52:56 +0200

    initial

---
+++ MyActorClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Actors;
+using Dapr.Actors.Client;
+using MyActor.Interfaces;
+
+namespace MyActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            InvokeActorMethodWithRemotingAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task InvokeActorMethodWithRemotingAsync()
+        {
+            var actorType = "MyActor";      // Registered Actor Type in Actor Service
+            var actorID = new ActorId("1");
+
+            // Create the local proxy by using the same interface that the service implements
+            // By using this proxy, you can call strongly typed methods on the interface using Remoting.
+            var proxy = ActorProxy.Create<IMyActor>(actorID, actorType);
+            var response = await proxy.SetDataAsync(new MyData()
+            {
+                PropertyA = "ValueA",
+                PropertyB = "ValueB",
+            });
+            Console.WriteLine(response);
+
+            var savedData = await proxy.GetDataAsync();
+            Console.WriteLine(savedData);
+        }
+    }
+}
---
+++ MyActorService/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Actors.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MyActorService
+{
+    public class Program
+    {
+        private const int AppChannelHttpPort = 3000;
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                        .UseActors(actorRuntime =>
+                        {
+                            // Register MyActor actor type
+                            actorRuntime.RegisterActor<MyActor>();
+                        })
+                        .UseUrls($"http://localhost:{AppChannelHttpPort}/");
+                });
+    }
+}
